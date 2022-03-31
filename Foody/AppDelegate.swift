3247b7bff25b17f62b2f480660b5ca5a1ed2feb2
Bilge Çakar <36795459+bilgecakar@users.noreply.github.com>
2022-03-31 22:31:11 +0300

    Add firebase sdk

--- Foody/AppDelegate.swift
+++ Foody/AppDelegate.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
